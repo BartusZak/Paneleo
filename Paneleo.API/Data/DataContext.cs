ddcce2b2230319b77bbe5a095bf1388ca6195321
BartusZak <b.ploszynski@gmail.com>
2018-10-25 16:10:50 +0200

    Initial Commit.

---
+++ Paneleo.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Paneleo.API.Models;
+
+namespace Paneleo.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
