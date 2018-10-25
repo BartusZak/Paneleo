977e36be3c2331cc31efe304fc8d09bc382cc407
BartusZak <b.ploszynski@gmail.com>
2018-10-25 16:24:31 +0200

    Adding UserModel and new migration.

---
+++ Paneleo.API/Models/User.cs
@@ -0,0 +1,10 @@
+namespace Paneleo.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PaswordHash { get; set; }
+        public byte[] PaswordSalt { get; set; }
+    }
+}
