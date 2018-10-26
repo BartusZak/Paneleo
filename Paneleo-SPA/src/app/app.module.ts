6695ac14925a371e8c4b25e70366974379329075
BartusZak <b.ploszynski@gmail.com>
2018-10-26 13:21:20 +0200

    Controling form.

--- Paneleo-SPA/src/app/app.module.ts
+++ Paneleo-SPA/src/app/app.module.ts
@@ -5,20 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      ValueComponent,
-      NavComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, ValueComponent, NavComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
