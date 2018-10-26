48dd4a85c2bb164552e37550407e91eac8550c6c
BartusZak <b.ploszynski@gmail.com>
2018-10-26 15:27:19 +0200

    Parent to child comunication.

--- Paneleo.API/Controllers/ValuesController.cs
+++ Paneleo.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace Paneleo.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
