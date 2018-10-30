a649e1ad872f5d183ab99feebb6d90ddc0eb2e31
BartusZak <b.ploszynski@gmail.com>
2018-10-30 14:20:51 +0100

    UserController. .NET Core

---
+++ Paneleo.API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Paneleo.API.Data;
+
+namespace Paneleo.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IPaneleoRepository _repo;
+        public UsersController(IPaneleoRepository repo)
+        {
+            _repo = repo;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _repo.GetUsers();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _repo.GetUser(id);
+            return Ok(user);
+        }
+    }
+}
