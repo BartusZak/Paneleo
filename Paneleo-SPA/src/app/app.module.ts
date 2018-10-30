f188253e9eda8ab714411afc0149cba2e83181ae
BartusZak <b.ploszynski@gmail.com>
2018-10-30 07:50:29 +0100

    Setting up Routes Angular.

--- Paneleo-SPA/src/app/app.module.ts
+++ Paneleo-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -11,14 +12,29 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
+import { UsersListComponent } from './users-list/users-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProductsComponent } from './products/products.component';
+import { appRoutes } from './routes';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    UsersListComponent,
+    DashboardComponent,
+    OrdersComponent,
+    ProductsComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AuthService, ErrorInterceptorProvider, AlertifyService],
   bootstrap: [AppComponent]
---
+++ Paneleo-SPA/src/app/routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProductsComponent } from './products/products.component';
+
+export const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'user-list', component: UsersListComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+];
