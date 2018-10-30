74c843853a4d8ad7b2a0a89f8ce32f602ac7bfa3
BartusZak <b.ploszynski@gmail.com>
2018-10-30 10:11:39 +0100

    Routing in components. Angular

--- Paneleo-SPA/src/app/left-menu/left-menu.component.ts
+++ Paneleo-SPA/src/app/left-menu/left-menu.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-menu.component.css']
 })
 export class LeftMenuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- Paneleo-SPA/src/app/routes.ts
+++ Paneleo-SPA/src/app/routes.ts
@@ -3,11 +3,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { UsersListComponent } from './users-list/users-list.component';
 import { OrdersComponent } from './orders/orders.component';
 import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
 
 export const appRoutes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'orders', component: OrdersComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'user-list', component: UsersListComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
 ];
