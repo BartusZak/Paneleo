453c44fd1cfb0cdd8b73a28d2b9214570639a710
BartusZak <b.ploszynski@gmail.com>
2018-12-31 23:48:57 +0100

    Dashboard. .NET Core

--- Paneleo.API/Paneleo.Services/Services/PaneleoDashboardService.cs
+++ Paneleo.API/Paneleo.Services/Services/PaneleoDashboardService.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using Paneleo.Data.Repository.Interfaces;
 using Paneleo.Models.Model.Contractor;
 using Paneleo.Models.Model.Order;
+using Paneleo.Models.Model.Product;
 using Paneleo.Models.ModelDto;
+using Paneleo.Models.ModelDto.Contractor;
 using Paneleo.Models.ModelDto.Dashboard;
+using Paneleo.Models.ModelDto.Order;
+using Paneleo.Models.ModelDto.Product;
 using Paneleo.Services.Interfaces;
 
 namespace Paneleo.Services.Services
@@ -16,11 +21,19 @@ namespace Paneleo.Services.Services
     {
         private readonly IRepository<OrderProduct> _orderProductRepository;
         private readonly IRepository<Contractor> _contractorRepository;
+        private readonly IRepository<Order> _orderRepository;
+        private readonly IRepository<Product> _productRepository;
 
-        public PaneleoDashboardService(IRepository<OrderProduct> orderProductRepository, IRepository<Contractor> contractorRepository)
+        private readonly IMapper _mapper;
+
+        public PaneleoDashboardService(IRepository<OrderProduct> orderProductRepository, IRepository<Contractor> contractorRepository, IRepository<Order> orderRepository, IRepository<Product> productRepository, IMapper mapper)
         {
             _orderProductRepository = orderProductRepository;
             _contractorRepository = contractorRepository;
+            _orderRepository = orderRepository;
+            _productRepository = productRepository;
+
+            _mapper = mapper;
 
         }
 
@@ -31,20 +44,51 @@ namespace Paneleo.Services.Services
                 SuccessResult = new DashboardStatisticsDto
                 {
                     TotalOrdersValue = await CalculateTotalOrdersValue(),
-                    ContractorsCount = await CountContactors()
+                    ContractorsCount = await CountContactors(),
+                    OrdersCount = await CountOrders(),
+                    ProductsCount = await CountProducts(),
+                    Contractors = await GetLast5Contractors(),
+                    Orders = await GetLast5Orders(),
+                    Products = await GetLast5Products()
                 }
             };
-
-
-
             return response;
+        }
+
+        private async Task<ICollection<ProductShortDto>> GetLast5Products()
+        {
+            var contractors = (await _productRepository.GetAllAsync()).OrderByDescending(x => x.CreatedAt).Take(5);
+
+            return _mapper.Map<ICollection<ProductShortDto>>(contractors);
+        }
+
+        private async Task<ICollection<OrderShortDto>> GetLast5Orders()
+        {
+            var contractors = (await _orderRepository.GetAllAsync()).OrderByDescending(x => x.CreatedAt).Take(5);
+
+            return _mapper.Map<ICollection<OrderShortDto>>(contractors);
+        }
+
+        private async Task<ICollection<ContractorShortDto>> GetLast5Contractors()
+        {
+            var contractors = (await _contractorRepository.GetAllAsync()).OrderByDescending(x => x.CreatedAt).Take(5);
+
+            return _mapper.Map<ICollection<ContractorShortDto>>(contractors);
+        }
+
+        private async Task<int> CountProducts()
+        {
+            return (await _productRepository.GetAllAsync()).Count();
+        }
+
+        private async Task<int> CountOrders()
+        {
+            return (await _orderRepository.GetAllAsync()).Count();
         }
 
         private async Task<int> CountContactors()
         {
-
-            return (await _contractorRepository.GetAllAsync()).ToList().Count();
-            
+            return (await _contractorRepository.GetAllAsync()).Count();
         }
 
         private async Task<double> CalculateTotalOrdersValue()
